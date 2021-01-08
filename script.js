325d8d9b94d5748d2852784780be6dea8fd0ea56
you know who <chorvickprocode@gmail.com>
2021-01-08 14:08:49 -0500

    put questions inside function - clean up/add comments

--- script.js
+++ script.js
@@ -9,39 +9,48 @@ var length = 0;
 var answer = "";/// make sure everything starts empty
 
 
-var length = prompt("Please enter a number between 8 and 128 for the length of your password.")
 
-///below we test for a number between 8 and 128 while loop works unless user inputs strings
-///improved check of user input but not perfect 
-
-while (length < 8 || length > 128) {
-    var length = prompt("SORRY - Please try again with a number between 8 and 128");
-}
-
-while (isNaN(length)) {
-    var length = prompt("SORRY - Please try again with a number between 8 and 128");
-}
-
-while (length == 0 || length == null) {
-    var length = prompt("SORRY - Please try again with a number between 8 and 128");
-}
-
-
-
-
-var selecthi = confirm("Should your password contain upper case letters ?");
-
-var selectlow = confirm("Should your password contain lower case letters ?");
-
-var selectnum = confirm("Should your password contain numbers ?");
-
-var selectsym = confirm("Should your password contain symbold and special charachters ?");
 ////
 
 
-/// make function to generate password based on the users selections
+/// make function to generate password based on the users selections  by putting the qualifying questions inside 
+///function now the prompts no longer show up as soon as you load the page,  
+/// for this assignment the pmompts and questions to user how they want their password
+//  should be inside this function so it starts to work when the button is pressed and
+/// so it does not generate multiple passwords if the button is pressed multiple times 
+
+
+///define our function pw which first asks questions then makes the password to user specifications
 
 function pw() {
+
+    var length = prompt("Please enter a number between 8 and 128 for the length of your password.")
+
+    ///below we test for a number between 8 and 128 while loop works unless user inputs strings
+    ///improved check of user input but not perfect 
+
+    while (length < 8 || length > 128) {
+        var length = prompt("SORRY - Please try again with a number between 8 and 128");
+    }
+
+    while (isNaN(length)) {
+        var length = prompt("SORRY - Please try again with a number between 8 and 128");
+    }
+
+    while (length == 0 || length == null) {
+        var length = prompt("SORRY - Please try again with a number between 8 and 128");
+    }
+
+
+
+
+    var selecthi = confirm("Should your password contain upper case letters ?");
+
+    var selectlow = confirm("Should your password contain lower case letters ?");
+
+    var selectnum = confirm("Should your password contain numbers ?");
+
+    var selectsym = confirm("Should your password contain symbold and special charachters ?");
 
     if (selecthi) {
         elementsof += upC;
@@ -76,8 +85,8 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button-------event listener broke program
-//adding test to not generate multiple passwords if button is pressed again and again
+//
+//
 
 
 
