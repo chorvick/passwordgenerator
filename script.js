945bcab305483d23ac58f12fd69ee3e942774930
you know who <chorvickprocode@gmail.com>
2021-01-08 20:58:34 -0500

    added test case if user selects 0 of 4 options

--- script.js
+++ script.js
@@ -10,15 +10,16 @@ var answer = "";/// make sure everything starts empty
 
 
 
-////
-
-
 /// make function to generate password based on the users selections  by putting the qualifying questions inside 
 ///function now the prompts no longer show up as soon as you load the page,  
 /// for this assignment the pmompts and questions to user how they want their password
 //  should be inside this function so it starts to work when the button is pressed and
 /// so it does not generate multiple passwords if the button is pressed multiple times 
 
+function confirm_inputs(inputs, condition) {
+    let trues = Object.keys(inputs).filter(key => inputs[key] == condition)
+    return trues
+}
 
 ///define our function pw which first asks questions then makes the password to user specifications
 
@@ -54,53 +55,40 @@ function pw() {
     var selectnum = confirm("Should your password contain numbers ?");
 
     var selectsym = confirm("Should your password contain symbold and special charachters ?");
-
-    /// adding test to make sure the user selected at least one of the four options , if all 
-    /// four evaluate false it notifies them and asks them to try again
-
-    // if ((selecthi) && (selectlow) && (selectnum) && (selectsym) == "false") {
-    //     alert("Sorry you have to pick at least one of the four conditions ! Please try again.");
-
-    //     var selecthi = confirm("Should your password contain upper case letters ?");
-
-    //     var selectlow = confirm("Should your password contain lower case letters ?");
-
-    //     var selectnum = confirm("Should your password contain numbers ?");
-
-    //     var selectsym = confirm("Should your password contain symbold and special charachters ?");
-    // }
+    //
+    // we test here to make sure the user selected at least one of the four conditions /parameters
+    //as long as they selected at least one choice a password will be produced
 
 
+    let conditions = confirm_inputs({ selecthi, selectlow, selectnum, selectsym }, true)
+    if (conditions.length == 0) {
 
-
-
-
-
-    if (selecthi) {
-        elementsof += upC;
+        return "Please try again and select at least one of the four parameters presented."
+    } else {
+        if (selecthi) {
+            elementsof += upC;
+        }
+        if (selectlow) {
+            elementsof += loC;
+        }
+        if (selectnum) {
+            elementsof += num;
+        }
+        if (selectsym) {
+            elementsof += spC;
+        }
+        ////  the for loop uses Math.random to put the password the user requested into 'answer'
+        for (i = 0; i < length; i++) {
+            answer += elementsof.charAt(Math.floor(Math.random() * elementsof.length));
+        }
+        return answer;
     }
-    if (selectlow) {
-        elementsof += loC;
-    }
-    if (selectnum) {
-        elementsof += num;
-    }
-    if (selectsym) {
-        elementsof += spC;
-    }
-    ////  the for loop uses Math.random to put the password the user requested into 'answer'
-    for (i = 0; i < length; i++) {
-        answer += elementsof.charAt(Math.floor(Math.random() * elementsof.length));
-    }
-    return answer;
+
 }
 
-
-
-
-
 //Write password to the #password input
-function writePassword() {
+function writePassword(event) {
+    console.log(event)
 
     var password = pw();
     var passwordText = document.querySelector("#password");
@@ -111,9 +99,6 @@ function writePassword() {
 
 //
 //
-
-
-
 
 generateBtn.addEventListener("click", writePassword);
 
